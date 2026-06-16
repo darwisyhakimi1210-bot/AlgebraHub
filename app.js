f3583438c9cfe7cf8c1ee4e34bcd9003e96f7cd9
Darwisy Hakimi <darwisyhakimi1210@gmail.com>
2026-06-17 01:09:26 +0800

    change svg

--- app.js
+++ app.js
@@ -119,7 +119,7 @@ function renderNav() {
   document.getElementById('navbar').innerHTML = `
     <button class="nav-brand" onclick="App.go('home')">
       <div class="nav-brand-icon">
-        <svg width="26" height="26" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2.2" stroke-linecap="round" stroke-linejoin="round"><path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"/><path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"/></svg>
+        <img src="image/svg.png" width="40" height="40" alt="AlgebraHub">
       </div>
       <div class="nav-brand-text">
         <div class="nav-brand-title"><span class="p">Algebra</span><span class="g">Hub</span></div>
